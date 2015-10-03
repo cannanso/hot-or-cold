579fc6f68d0aadaa0492b650aa0d097cc68ce897
Claire Annan <claire.annan.me@gmail.com>
2015-10-02 20:52:10 -0400

    Code to generate random secret number and to validate user guess.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,55 @@
 
 $(document).ready(function(){
-	
+
+	function newGame() {
+		var count = 0,
+				guess = '',
+				random = secret(),
+				list = '';
+
+
+		
+		console.log('Let the games begin!');
+		console.log('Secret Number is ' + random);
+
+		function secret() {
+			return Math.floor(Math.random() * 100) + 1;
+		}
+
+		function validate(guess) {
+			// check if guess supplied a numeric input between 1 and 100.
+			if (guess === parseInt(guess, 10)) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		function upCount() {
+			// increase count of number of guesses.
+			count++;
+			$('#count').text(count);
+		}
+
+		function addList(guess) {
+			// append each guessed number as an <li> to ul#guessList
+		}	
+
+		function feedback() {
+			// call secret() to set var random
+			// change var guess to input#userGuess.val()
+			// validate(guess) 
+			// if/else ranges for guess
+			// call upCount() to set var count
+			// addList(guess)
+			// later version: call guess() to compare guess with random number
+			// return ice cold, cold, warm, hot, burnin hot
+			return 'cold';
+		}		
+
+	}
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,49 +61,15 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	function newGame() {
-  		var count = 0;
-  		var guess = '';
-  		var random = '';
-  		var list = '';
+  	/*--- Start a new game ---*/
+  	$(".new").click(newGame);
 
-			function secret() {
-				// generate random number(Math.random)
-				// return random number
-			}
-
-			function feedback() {
-				// change var guess to input#userGuess.val()
-				// validate(guess) 
-				// if/else ranges for guess
-				// call upCount() to set var count
-				// addList(guess)
-				// call secret() to set var random
-				// later version: call guess() to compare guess with random number
-				// return ice cold, cold, warm, hot, burnin hot
-			}
-
-			function validate(guess) {
-				// check if guess supplied a numeric input between 1 and 100.
-				// parseInt();
-			}
-
-			function upCount() {
-				// increase count of number of guesses.
-				// count++;
-				// change content of span#count.
-			}
-
-			function addList(guess) {
-				// append each guessed number as an <li> to ul#guessList
-			}
-			
-
-  	}
-
-  	// click a.new to start a new game
-  	// click input#guessButton to get feedback and change div#feedback.text()
-  	
+		// click input#guessButton to get feedback and change div#feedback.text()	
+		$('#guessButton').click(function(e){
+			e.preventDefault();
+			//var feedback = feedback();
+			$('#feedback').text('cold');
+		});  	  	
 
 });
 
